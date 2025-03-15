87e14c54a9642b6b8a4134f0a69eba947ddf4d6b
Валерий <valeramihalev628@gmail.com>
2025-03-15 22:48:44 +0300

    Add files via upload

---
+++ car/src/Main.java
@@ -0,0 +1,13 @@
+public class Car {
+
+    static void String mulString(String p, int n) {
+        String result = ""
+                for(int i = 0; i < n; i++)
+                    result += p;
+                return = result;
+    }
+    public class void main(String[] args){
+
+        System.out.println("A", 100);
+    }
+}
